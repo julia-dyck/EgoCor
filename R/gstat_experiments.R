0e634168f651bc647e523f7bd69facb49dd5a8e0
julia-dyck <julia.alexandra.dyck@gmail.com>
2022-05-17 16:49:46 +0200

    experiments continued

--- R/gstat_experiments.R
+++ R/gstat_experiments.R
@@ -1,29 +1,44 @@
-### experiment with gstat package
-
-install.packages("gstat")
-
-str(birth)
-coordinates(birth) = ~x+y
-v = gstat::variogram(object = birthweight ~ 1, data = birth, cutoff = 500, width = 50)
-
-plot(v)
-summary(v)
-
-
-
-
-
 
 
 
 # ------------------------------------------
 # geor estimation for comparison reasons
+library(EgoCor)
+
+v.egocor = vario.mod(data = birth, max.dist = 500, nbins = 10, shinyresults = F, windowplots = T)
+variog.egocor = v.egocor$variog.list[[1]]
+variog.egocor
+
+vmod.egocor = v.egocor$vmod.list
+vmod.egocor
 
 
-v01 = vario.mod(data = birth) # variomod fct does not work ...why?
+### experiment with gstat package
+
+install.packages("gstat")
+library(gstat)
+
+str(birth)
+birth.reformatted = birth
+sp::coordinates(birth.reformatted) = ~x+y
+v = gstat::variogram(object = birthweight ~ 1, data = birth.reformatted, cutoff = 500, width = 50)
 
 
 
+# graphical comparison of emp. sv
+plot(variog.egocor$uvec, variog.egocor$v, xlim = c(0,520), ylim = c(0, 300000))
+abline(v = variog.egocor$uvec, col = 5, lty = 3) # middle of lag acc. to geor estimate
+abline(v=v$dist, col = 3, lty = 2) # middle of lag acc. to gstat estimate
+
+plot(v)
+# cannot add lines to the gstat plot
+
+
+
+
+
+# -------------------------------
+## example for input values
 library(sp)
 data(meuse)
 
