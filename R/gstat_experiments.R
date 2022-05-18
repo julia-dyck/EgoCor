9cf36e0ac23aee773ac18be64655212b637e7777
julia-dyck <julia.alexandra.dyck@gmail.com>
2022-05-18 11:49:27 +0200

    complete example: semivariogram estimation and model fitting with geor vs gstat

--- R/gstat_experiments.R
+++ R/gstat_experiments.R
@@ -9,41 +9,49 @@ v.egocor = vario.mod(data = birth, max.dist = 500, nbins = 10, shinyresults = F,
 variog.egocor = v.egocor$variog.list[[1]]
 variog.egocor
 
-vmod.egocor = v.egocor$vmod.list
+vmod.egocor = v.egocor$vmod.list[[1]]
 vmod.egocor
 
 
 ### experiment with gstat package
 
-install.packages("gstat")
+#install.packages("gstat")
 library(gstat)
+
 
 str(birth)
 birth.reformatted = birth
 sp::coordinates(birth.reformatted) = ~x+y
-v = gstat::variogram(object = birthweight ~ 1, data = birth.reformatted, cutoff = 500, width = 50)
+variog.gstat = gstat::variogram(object = birthweight ~ 1, data = birth.reformatted, cutoff = 500, width = 50)
+
+# prep exponential model (with or without starting values for the fitting):
+v <- vgm("Exp")                        # variogram model (without starting values)
+v2 <- vgm(psill = var(birth$birthweight),  # plausible starting value for psill = sample variance
+          model = "Exp",
+          range = 500/3,     # plausible starting value for range: maxdist/3
+          nugget = 0)      # plausible starting value for nugget
+
+vfit.gstat = fit.variogram(variog.gstat, model=v2,  # fitting the model with starting model
+                      fit.sills = TRUE,
+                      fit.ranges = TRUE,
+                      fit.method = 7,
+                      debug.level = 1, warn.if.neg = FALSE, fit.kappa = FALSE)
+vfit.gstat
 
 
 
-# graphical comparison of emp. sv
+### graphical comparison of emp. sv
 plot(variog.egocor$uvec, variog.egocor$v, xlim = c(0,520), ylim = c(0, 300000))
 abline(v = variog.egocor$uvec, col = 5, lty = 3) # middle of lag acc. to geor estimate
-abline(v=v$dist, col = 3, lty = 2) # middle of lag acc. to gstat estimate
+abline(v=variog.gstat$dist, col = 3, lty = 2) # middle of lag acc. to gstat estimate
 
-plot(v)
-# cannot add lines to the gstat plot
-
+plot(variog.gstat)
 
 
+### graphical comparison of the sv model
+plot(variog.egocor$uvec, variog.egocor$v, xlim = c(0,520), ylim = c(0, 300000))
+graphics::lines(vmod.egocor)
 
+plot(variog.gstat)
+plot(variog.gstat, vfit.gstat, cutoff=500)
 
-# -------------------------------
-## example for input values
-library(sp)
-data(meuse)
-
-
-head(meuse)
-class(meuse)
-coordinates(meuse) = ~x+y
-gstat::variogram(log(zinc)~1, meuse)
