db6577fbb37c9b6488e8a2ca68672254a9f4a053
Jan-Ole Koslik <janole.koslik@gmail.com>
2022-06-20 13:22:01 +0200

    fixing the data argument

--- R/sv.sep.R
+++ R/sv.sep.R
@@ -7,9 +7,12 @@ sv.sep = function(data, coords, max.dist, nbins){
   # emp.sv = geoR::variog(coords=coords, data=data, estimator.type="classical",
   #                     max.dist = max.dist, uvec = nbins,
   #                      messages = F)
-  emp.sv = gstat::variogram(object = data[[1]] ~ 1, data = data, cutoff = max.dist, width = max.dist / nbins)
-  ini.partial.sill <- stats::var(data)
-  ini.shape <- emp.sv$max.dist/3
+  data.ge = as.data.frame(cbind(coords,data))
+  colnames(data.ge)[1:2] = c("x", "y")
+  sp::coordinates(data.ge) = ~x+y
+  emp.sv = gstat::variogram(object = data.ge[[1]] ~ 1, data = data.ge, cutoff = max.dist, width = max.dist / nbins)
+  ini.partial.sill <- stats::var(data.ge[[1]])
+  ini.shape <- max(emp.sv$dist)/3
   ini.values <- c(ini.partial.sill, ini.shape)
   v = gstat::vgm(psill = ini.partial.sill, model = "Exp", range = ini.shape, nugget = 0)
   #sv.mod <- geoR::variofit(emp.sv, ini.cov.pars = ini.values,
