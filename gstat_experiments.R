52ac90b3ed9338719092784764be2d6fc69f7905
julia dyck <julia.alexandra.dyck@gmail.com>
2022-06-24 14:29:04 +0200

    comparison of sse computed manually with sse according to fit.variogram output for every fit.method

--- gstat_experiments.R
+++ gstat_experiments.R
@@ -34,24 +34,73 @@ v2 <- vgm(psill = var(birth$birthweight),  # plausible starting value for psill 
 vfit.gstat = fit.variogram(variog.gstat, model=v2,  # fitting the model with starting model
                       fit.sills = TRUE,
                       fit.ranges = TRUE,
-                      fit.method = 7,
+                      fit.method = 2,
                       debug.level = 1, warn.if.neg = FALSE, fit.kappa = FALSE)
 vfit.gstat
+plot(variog.gstat, vfit.gstat, cutoff=1000)
 
+attr(vfit.gstat,"SSErr") # sum of squared errors (= loss function value)
+
+### manually compute the SSEs (= loss function value) :
+# to verify the weights in the loss function
+exp.sv = function(lag, nugget, psill, phi){
+  ret = nugget + psill*(1-exp(-lag/phi))
+  return(ret)
+}
+
+gamma.mod = exp.sv(lag = variog.gstat$dist, nugget = vfit.gstat$psill[1], vfit.gstat$psill[2], phi = vfit.gstat$range)
+
+weig = variog.gstat$np                       # if fit.method = 1 :)
+weig = variog.gstat$np/variog.gstat$gamma^2  # if fit.method = 2 :)
+# fit method 3 - 5 not implemented for fit.variogram
+weig = rep(1, length(variog.gstat$np))       # if fit.method = 6 :)
+
+weig = variog.gstat$np/variog.gstat$dist^2   # if fit.method = 7 :(
+
+sse = sum(weig*(variog.gstat$gamma - gamma.mod)^2)
+sse
+# compare to
+attr(vfit.gstat,"SSErr") # sum of squared errors acc. to gstat output
 
 
 ### graphical comparison of emp. sv
 plot(variog.egocor$uvec, variog.egocor$v, xlim = c(0,520), ylim = c(0, 300000))
 abline(v = variog.egocor$uvec, col = 5, lty = 3) # middle of lag acc. to geor estimate
 abline(v=variog.gstat$dist, col = 3, lty = 2) # middle of lag acc. to gstat estimate
+plot(vmod.egocor)
+
+
 
 plot(variog.gstat)
 
 
 ### graphical comparison of the sv model
+
+# geor
 plot(variog.egocor$uvec, variog.egocor$v, xlim = c(0,520), ylim = c(0, 300000))
 graphics::lines(vmod.egocor)
 
-plot(variog.gstat)
-plot(variog.gstat, vfit.gstat, cutoff=500)
+curve(vmod.egocor$nugget + vmod.egocor$cov.pars[1]*(1 - exp(-x/vmod.egocor$cov.pars[2])),
+      from = 0, to = 500, ylim = c(0,230000))
+abline(v = -log(0.05*(1+(vmod.egocor$nugget/vmod.egocor$cov.pars[1])))*vmod.egocor$cov.pars[2], col = "blue")
+abline(v = geoR::practicalRange("exp", vmod.egocor$cov.pars[2]), col = "red")
+abline(h = 0.95*(vmod.egocor$nugget + vmod.egocor$cov.pars[1]), col = "blue")
 
+
+
+
+# gstat
+abline(v = -log(0.05)*vfit.gstat$range[2], col = "blue")
+abline(h = 0.95*sum(vfit.gstat$psill), col = "blue")
+
+plot(variog.gstat)
+plot(variog.gstat, vfit.gstat, cutoff=1000)
+
+
+curve(vfit.gstat$psill[1] + vfit.gstat$psill[2]*(1 - exp(-x/vfit.gstat$range[2])),
+      from = 0, to = 500)
+
+abline(v = -log(0.05*(1+(vfit.gstat$psill[1]/(vfit.gstat$psill[2]))))*vfit.gstat$range[2], col = "blue")
+abline(h = 0.95*sum(vfit.gstat$psill), col = "blue")
+prac.range.value = -log(0.05*(1+(vfit.gstat$psill[1]/(vfit.gstat$psill[2]))))*vfit.gstat$range[2]
+prac.range.value
--- gstat_fit.method_experiments.R
+++ gstat_fit.method_experiments.R
@@ -1,54 +1,125 @@
-##
+## Goal:
+## Verify the weights in the loss function according to gstat::fit.variogram
+## documentation details regarding fit.method argument
 
 library(gstat)
+library(sp)
 
-
-str(birth)
-birth.reformatted = birth
-sp::coordinates(birth.reformatted) = ~x+y
-variog.gstat = gstat::variogram(object = birthweight ~ 1, data = birth.reformatted, cutoff = 500, width = 50)
-
-# prep exponential model (with or without starting values for the fitting):
-v <- vgm("Exp")                        # variogram model (without starting values)
-v2 <- vgm(psill = var(birth$birthweight),  # plausible starting value for psill = sample variance
-          model = "Exp",
-          range = 500/3,     # plausible starting value for range: maxdist/3
-          nugget = 0)      # plausible starting value for nugget
-
-vfit.gstat = fit.variogram(variog.gstat, model=v2,  # fitting the model with starting model
-                           fit.sills = TRUE,
-                           fit.ranges = TRUE,
-                           fit.method = 7,
-                           debug.level = 1, warn.if.neg = FALSE, fit.kappa = FALSE)
-vfit.gstat
-plot(variog.gstat, vfit.gstat, cutoff=1000)
-
-attr(vfit.gstat,"SSErr") # sum of squared errors (= loss function value)
-
-### manually compute the SSEs (= loss function value) :
-# to verify the weights in the loss function
-exp.sv = function(lag, nugget, psill, phi){
+exp.sv = function(lag, nugget, psill, phi){ #exp. sv. model values for lag distance
   ret = nugget + psill*(1-exp(-lag/phi))
   return(ret)
 }
 
-gamma.mod = exp.sv(lag = variog.gstat$dist, nugget = vfit.gstat$psill[1], vfit.gstat$psill[2], phi = vfit.gstat$range)
+# spatial sample
+data(meuse)
 
-weig = variog.gstat$np                       # if fit.method = 1 :)
-weig = variog.gstat$np/variog.gstat$gamma^2  # if fit.method = 2 :)
-# fit method 3 - 5 not implemented for fit.variogram
-weig = rep(1, length(variog.gstat$np))       # if fit.method = 6 :)
+sp::coordinates(meuse) = ~x+y
 
-weig = variog.gstat$np/variog.gstat$dist^2   # if fit.method = 7 :(
+# fit empirical semivariogram model
+variog = gstat::variogram(object = log(zinc) ~ 1, data = meuse, cutoff = 1500, width = 150)
 
-sse = sum(weig*(variog.gstat$gamma - gamma.mod)^2)
-sse
+# prep exponential model
+v <- vgm(psill = var(log(meuse$zinc)),  # plausible starting value for psill = sample variance
+          model = "Exp",
+          range = 1500/3,     # plausible starting value for range: maxdist/3
+          nugget = 0)      # plausible starting value for nugget
+
+# ---------------------------------------------------------------------------------
+
+# Try out different fit methods:
+
+##################
+# fit.method = 1 #
+##################
+vfit = fit.variogram(variog, model=v,  # fitting the model with starting model
+                           fit.sills = TRUE,
+                           fit.ranges = TRUE,
+                           fit.method = 1,
+                           debug.level = 1, warn.if.neg = FALSE, fit.kappa = FALSE)
+vfit
+plot(variog, vfit, cutoff=1500)
+
+# manually compute the SSEs (= loss function value)
+gamma.mod = exp.sv(lag = variog$dist, nugget = vfit$psill[1], psill = vfit$psill[2], phi = vfit$range[2])
+
+
+
+weig = variog$np                       # for fit.method = 1
+sse1 = sum(weig*(variog$gamma - gamma.mod)^2)
+sse1
 # compare to
-attr(vfit.gstat,"SSErr") # sum of squared errors acc. to gstat output
+sse1.fit = print(attr(vfit,"SSErr")) # sum of squared errors acc. to gstat output
+
+##################
+# fit.method = 2 #
+##################
+vfit = fit.variogram(variog, model=v,  # fitting the model with starting model
+                     fit.sills = TRUE,
+                     fit.ranges = TRUE,
+                     fit.method = 2,
+                     debug.level = 1, warn.if.neg = FALSE, fit.kappa = FALSE)
+vfit
+plot(variog, vfit, cutoff=1500)
+
+# manually compute the SSEs (= loss function value)
+gamma.mod = exp.sv(lag = variog$dist, nugget = vfit$psill[1], psill = vfit$psill[2], phi = vfit$range[2])
+
+weig = variog$np/variog$gamma^2  # if fit.method = 2
+sse2 = sum(weig*(variog$gamma - gamma.mod)^2)
+# compare to
+sse2.fit = print(attr(vfit,"SSErr")) # sum of squared errors acc. to gstat output
 
 
 
+##################
+# fit.method = 6 #
+##################
+vfit = fit.variogram(variog, model=v,  # fitting the model with starting model
+                     fit.sills = TRUE,
+                     fit.ranges = TRUE,
+                     fit.method = 6,
+                     debug.level = 1, warn.if.neg = FALSE, fit.kappa = FALSE)
+vfit
+plot(variog, vfit, cutoff=1500)
 
+# manually compute the SSEs (= loss function value)
+gamma.mod = exp.sv(lag = variog$dist, nugget = vfit$psill[1], psill = vfit$psill[2], phi = vfit$range[2])
+
+weig = rep(1, length(variog$np))       # if fit.method = 6
+sse6 = sum(weig*(variog$gamma - gamma.mod)^2)
+# compare to
+sse6.fit = print(attr(vfit,"SSErr")) # sum of squared errors acc. to gstat output
+
+
+
+##################
+# fit.method = 7 #
+##################
+vfit = fit.variogram(variog, model=v,  # fitting the model with starting model
+                     fit.sills = TRUE,
+                     fit.ranges = TRUE,
+                     fit.method = 7,
+                     debug.level = 1, warn.if.neg = FALSE, fit.kappa = FALSE)
+vfit
+plot(variog, vfit, cutoff=1500)
+
+# manually compute the SSEs (= loss function value)
+gamma.mod = exp.sv(lag = variog$dist, nugget = vfit$psill[1], psill = vfit$psill[2], phi = vfit$range[2])
+
+weig = variog$np/variog$dist^2   # if fit.method = 7
+sse7 = sum(weig*(variog$gamma - gamma.mod)^2)
+# compare to
+sse7.fit = print(attr(vfit,"SSErr")) # sum of squared errors acc. to gstat output
+
+##################
+# sse comparison #
+##################
+
+sses = matrix(c(sse1, sse2, sse6, sse7, sse1.fit, sse2.fit, sse6.fit, sse7.fit), ncol = 2, byrow = F)
+sses = data.frame(sses)
+colnames(sses) = c("manually calculated SSE", "SSE acc. to fit.variogram output")
+
+sses
 
 
 
