2474631b5c63dc90633982d39f72725a36412247
julia-dyck <julia.alexandra.dyck@gmail.com>
2023-07-17 20:03:29 +0200

    update manual pdf

--- manual_creation_for_RPackages.R
+++ manual_creation_for_RPackages.R
@@ -1,5 +1,5 @@
 
 system("R CMD Rd2pdf C:/Users/jdyck/github_projects_office_access/EgoCor") # uni office
 
-system("R CMD Rd2pdf D:/github_projects_homeoffice_access/EgoCor") # home office
+system("R CMD Rd2pdf D:/J/github_homeoffice_access/EgoCor") # home office
 
