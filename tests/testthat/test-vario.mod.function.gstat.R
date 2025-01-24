c252d4eedd1ef9929ddb47aa01ac6c80483fd077
Dyck <j.dyck@uni-bielefeld.de>
2025-01-24 20:07:56 +0100

    add tests back in

--- tests/testthat/test-vario.mod.function.gstat.R
+++ tests/testthat/test-vario.mod.function.gstat.R
@@ -9,24 +9,24 @@
 # test input validation
 
 
-# test_that("vario.mod throws error if max.dist and nbins input do not match", {
-#   expect_error(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = c(100, 200), nbins = c(10,11,12)),
-#                "If vectors for both input parameters max.dist and nbins are specified, they must have the same length")
-#   expect_error(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = c(100, 200, 300), nbins = c(10,11)),
-#                "If vectors for both input parameters max.dist and nbins are specified, they must have the same length")
-#
-# })
-#
-# test_that("vario.mod works with single max.dist and nbins", {
-#   result <- suppressWarnings(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = 1000, nbins = 5, shinyresults = F))
-#   expect_equal(nrow(result$infotable), 1)
-# })
-#
-# test_that("vario.mod works with multiple max.dist and nbins", {
-#   result <- suppressWarnings(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = c(1000, 2000), nbins = c(5, 10), shinyresults = F))
-#   expect_equal(nrow(result$infotable), 2)
-# })
-#
+test_that("vario.mod throws error if max.dist and nbins input do not match", {
+  expect_error(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = c(100, 200), nbins = c(10,11,12)),
+               "If vectors for both input parameters max.dist and nbins are specified, they must have the same length")
+  expect_error(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = c(100, 200, 300), nbins = c(10,11)),
+               "If vectors for both input parameters max.dist and nbins are specified, they must have the same length")
+
+})
+
+test_that("vario.mod works with single max.dist and nbins", {
+  result <- suppressWarnings(vario.mod(data = birth[,1:3], max.dist = 600, nbins = 12, shinyresults = F))
+  expect_equal(nrow(result$infotable), 1)
+})
+
+test_that("vario.mod works with multiple max.dist and nbins", {
+  result <- suppressWarnings(vario.mod(data = birth[,1:3], max.dist = c(500, 600), nbins = c(10, 11), shinyresults = F))
+  expect_equal(nrow(result$infotable), 2)
+})
+
 #
 # # output validation
 #
