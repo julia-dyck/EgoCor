31f632427c7f91386d435c3debaa7e27e84bf681
Dyck <j.dyck@uni-bielefeld.de>
2025-01-24 15:17:42 +0100

    include one variomod test

--- tests/testthat/test-vario.mod.function.gstat.R
+++ tests/testthat/test-vario.mod.function.gstat.R
@@ -8,14 +8,14 @@
 
 # test input validation
 
-#
-# test_that("vario.mod throws error if max.dist and nbins input do not match", {
-#   expect_error(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = c(100, 200), nbins = c(10,11,12)),
-#                "If vectors for both input parameters max.dist and nbins are specified, they must have the same length")
-#   expect_error(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = c(100, 200, 300), nbins = c(10,11)),
-#                "If vectors for both input parameters max.dist and nbins are specified, they must have the same length")
-#
-# })
+
+test_that("vario.mod throws error if max.dist and nbins input do not match", {
+  expect_error(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = c(100, 200), nbins = c(10,11,12)),
+               "If vectors for both input parameters max.dist and nbins are specified, they must have the same length")
+  expect_error(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = c(100, 200, 300), nbins = c(10,11)),
+               "If vectors for both input parameters max.dist and nbins are specified, they must have the same length")
+
+})
 #
 # test_that("vario.mod works with single max.dist and nbins", {
 #   result <- suppressWarnings(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = 1000, nbins = 5, shinyresults = F))
@@ -39,4 +39,4 @@
 # test_that("vario.mod returns output with correct output elements", {
 #   expect_named(suppressWarnings(vario.mod(data = birth, shinyresults = F)), c("infotable", "variog.list", "vmod.list", "input.arguments", "call"))
 # })
-#
+
