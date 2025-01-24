8c84a9331597ee3f6c295b74a346175367f83f2a
Dyck <j.dyck@uni-bielefeld.de>
2025-01-24 14:13:55 +0100

    check prep without these tests

--- tests/testthat/test-vario.mod.function.gstat.R
+++ tests/testthat/test-vario.mod.function.gstat.R
@@ -8,35 +8,3 @@
 
 # test input validation
 
-test_that("vario.mod throws error if max.dist and nbins input do not match", {
-  expect_error(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = c(100, 200), nbins = c(10,11,12)),
-               "If vectors for both input parameters max.dist and nbins are specified, they must have the same length")
-  expect_error(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = c(100, 200, 300), nbins = c(10,11)),
-               "If vectors for both input parameters max.dist and nbins are specified, they must have the same length")
-
-})
-
-test_that("vario.mod works with single max.dist and nbins", {
-  result <- suppressWarnings(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = 1000, nbins = 5, shinyresults = F))
-  expect_equal(nrow(result$infotable), 1)
-})
-
-test_that("vario.mod works with multiple max.dist and nbins", {
-  result <- suppressWarnings(vario.mod(data = data.frame(x = 1:10, y = 1:10, z = 1:10), max.dist = c(1000, 2000), nbins = c(5, 10), shinyresults = F))
-  expect_equal(nrow(result$infotable), 2)
-})
-
-
-# output validation
-
-
-test_that("vario.mod returns output with correct class", {
-  expect_s3_class(suppressWarnings(vario.mod(data = birth, shinyresults = F)), "vario.mod.output")
-})
-
-
-test_that("vario.mod returns output with correct output elements", {
- expect_named(suppressWarnings(vario.mod(data = birth, shinyresults = F)), c("infotable", "variog.list", "vmod.list", "input.arguments", "call"))
-})
-
-
