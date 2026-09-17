5dd0e6e4ac6d824b13da361f7a1987ad4120544b
Dyck <j.dyck@uni-bielefeld.de>
2026-09-17 16:00:27 +0200

    add sticker

---
+++ make_sticker.R
@@ -0,0 +1,25 @@
+### package sticker
+
+# Motiv herstellen:
+
+
+
+
+library(hexSticker)
+
+sticker(
+  subplot = "sticker_ChatGPT_Image2026-09-17.png",   # or .svg
+  p_color = "black",
+  s_x = 1,
+  s_y = 0.9,
+  s_width = 0.65,
+  package = "EgoCor",
+  p_size = 16,
+  p_y = 1.5,
+  h_color = "black",
+  h_fill = "#F8D0D8",
+  filename = "Logo_EgoCor.png",
+  dpi = 300
+)
+
+
